56942699cd314e5d68106b1f1dd6727d687bf0c7
pengpengduduguagua <2979468570@qq.com>
2019-01-18 17:26:35 +0800

    Initial Commit

---
+++ AttentionButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AttentionButton
+//
+//  Created by admin on 2019/1/18.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AttentionButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AttentionButton
+//
+//  Created by admin on 2019/1/18.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
